dc487e8f8904d8d256b8e9ab4f19a63488c334bc
rewledp <=>
2026-07-13 13:17:10 +0200

    added many stuff including rpc

---
+++ lua/plugins/lsp/mason.lua
@@ -0,0 +1,17 @@
+return {
+    {
+	"mason-org/mason-lspconfig.nvim",
+	opts = {
+	    ensure_installed = {
+		"rust_analyzer",
+		"lua_ls",
+	    }
+	},
+
+	dependencies = {
+	    { "mason-org/mason.nvim", opts = {} },
+	    "neovim/nvim-lspconfig",
+	},
+    },
+    "neovim/nvim-lspconfig"
+}

07dd734cb3310142b103873fb3eaa6024b776d8d
rewledp <=>
2026-07-15 14:49:58 +0200

    added harpoon

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Oil)
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], {noremap = true})
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,12 @@ vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
+
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+})
+

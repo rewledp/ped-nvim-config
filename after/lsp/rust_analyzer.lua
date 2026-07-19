2fc6b19cc3bf127c872725fee8cb9cc72eae5f03
rewledp <=>
2026-07-19 13:59:48 +0200

    added go to implementations keybinds

--- after/lsp/rust_analyzer.lua
+++ after/lsp/rust_analyzer.lua
@@ -1,7 +1,9 @@
 return {
     on_attach = function(client, bufnr)
-	if client.server_capabilities then
-	    vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-	end
+	vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+
+	-- if client:supports_method('textDocument/codeLens') then	
+	--     vim.lsp.codelens.enable(true, { bufnr = bufnr })
+	-- end
     end
 }
--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,4 +1,15 @@
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>cd", vim.cmd.Oil)
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], {noremap = true})
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+
+vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, { desc = 'List implementations' })
+
+--#region plugin dependant
+
+-- vim.keymap.set('n', '<leader>gi', function()
+--     require('telescope.builtin').lsp_implementations()
+-- end, { desc = 'List implementations' })
+
+--#endregion

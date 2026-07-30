d2802bcb559702d45066f2379a456e571be4e498
rewledp <=>
2026-07-30 17:40:17 +0200

    added copy-tree thingy (no i dont vibecode) and 'noice'

---
+++ lua/plugins/copytree.lua
@@ -0,0 +1,5 @@
+return {
+	"AdiY00/copy-tree.nvim",
+	cmd = "CopyTree",
+	opts = {},
+}
---
+++ lua/plugins/noice.lua
@@ -0,0 +1,30 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		lsp = {
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true,
+			},
+		},
+		presets = {
+			long_message_to_split = true,
+			inc_rename = false,
+			lsp_doc_border = true,
+		},
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		{
+			"rcarriga/nvim-notify",
+			opts = {
+				stages = "static",
+				timeout = 3000,
+				render = "compact",
+				top_down = true,
+			},
+		},
+	},
+}

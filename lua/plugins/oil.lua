e275871fc4955f2c38906f8abe06499806c2053e
rewledp <=>
2026-08-10 01:39:11 +0200

    added Oil yank keymap

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,9 +1,23 @@
 return {
 	"stevearc/oil.nvim",
-	---@module 'oil'
-	---@type oil.SetupOpts
-	opts = {},
-	-- Optional dependencies
+
+	opts = {
+		keymaps = {
+			["gy"] = {
+				desc = "Copy filepath to system clipboard",
+				callback = function()
+					local oil = require("oil")
+					local entry = oil.get_cursor_entry()
+					local dir = oil.get_current_dir()
+					if entry and dir then
+						vim.fn.setreg("+", dir .. entry.name)
+						vim.notify(dir .. entry.name)
+					end
+				end,
+			},
+		},
+	},
+
 	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
 	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
 	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.

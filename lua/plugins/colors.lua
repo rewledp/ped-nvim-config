ae3791f10294b98469b6ba1c9cdc52094d42f145
rewledp <=>
2026-08-12 14:17:50 +0200

    added themes, mini, quick session etc

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,35 +1,49 @@
 local function enable_transparency()
 	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 end
-
 return {
 	{ "ellisonleao/gruvbox.nvim" },
 	{ "EdenEast/nightfox.nvim" },
 	{ "sainnhe/everforest" },
 	{ "navarasu/onedark.nvim" },
 	{ "folke/tokyonight.nvim" },
-
+	{ "catppuccin/nvim", name = "catppuccin" },
+	{ "rebelot/kanagawa.nvim" },
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{ "Mofiqul/dracula.nvim" },
+	{ "marko-cerovac/material.nvim" },
+	{ "Shatur/neovim-ayu" },
+	{ "shaunsingh/nord.nvim" },
 	{
 		"zaldih/themery.nvim",
 		lazy = false,
 		opts = {
-			themes = { "tokyonight", "gruvbox", "nightfox", "everforest", "onedark" },
+			themes = {
+				"tokyonight",
+				"gruvbox",
+				"nightfox",
+				"everforest",
+				"onedark",
+				"catppuccin",
+				"kanagawa",
+				"rose-pine",
+				"dracula",
+				"material",
+				"ayu",
+				"nord",
+			},
 			themeConfigFile = "c:\\v:null",
 		},
-
 		config = function(_, opts)
 			require("themery").setup(opts)
-
 			vim.api.nvim_create_autocmd("ColorScheme", {
 				callback = function()
 					enable_transparency()
 				end,
 			})
-
 			enable_transparency()
 		end,
 	},
-
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
---
+++ lua/plugins/mini_move.lua
@@ -0,0 +1,5 @@
+return {
+	"echasnovski/mini.move",
+	version = "*",
+	opts = {},
+}
